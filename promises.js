41733959a26d5da7aadc1ebdeffbc442db22fa1d
Rakshanda wankhade <rakshandawankhade95@gmail.com>
2022-09-27 17:08:54 +0530

    Task 6 delivarable 1

--- promises.js
+++ promises.js
@@ -2,38 +2,30 @@ const posts = [
   {
     title: "Post 1",
     body: "This is post 1",
-    createdAt: new Date().getTime(),
   },
 
   {
     title: "Post 2",
     body: "This is post 2",
-    createdAt: new Date().getTime(),
   },
 ];
 
-let intervalId = 0;
-
 function getPost() {
-  clearInterval(intervalId);
-  intervalId = setInterval(() => {
+  setTimeout(() => {
     let output = " ";
-    for (let i = 0; i < posts.length; i++) {
-      output += `<li>${posts[i].title}   created ${
-        (new Date().getTime() - posts[i].createdAt) / 1000
-      }  seconds ago</li>`;
-    }
-    // console.log("timer running id=", timerId);
+    posts.forEach((post, index) => {
+      output += `<li>${posts[i].title}</li>`;
+    });
+
     document.body.innerHTML = output;
   }, 1000);
 }
 
-function createPost(post, callback) {
+function createPost(post) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      posts.push({ ...post, createdAt: new Date().getTime() });
-
-      const error = false;
+      posts.push(post);
+      const error = true;
       if (!error) {
         resolve();
       } else {
@@ -42,38 +34,6 @@ function createPost(post, callback) {
     }, 2000);
   });
 }
-
-function create4thPost(post, callback) {
-  setTimeOut(() => {
-    posts.push({ ...post, createdAt: new Date().getTime() });
-    callback();
-  }, 6000);
-}
-
-getPost();
-
-createPost(
-  {
-    title: "Post 3",
-    body: "This is post 3",
-  },
-  getPost()
-);
-createPost(
-  {
-    title: "Post 4",
-    body: "This is post 4",
-  },
-  getPost()
-);
-
-var timer;
-var count = 0;
-function lastEditedInSecondsAgo() {
-  count = 0;
-  clearInterval(timer);
-  timer = setInterval(() => {
-    count++;
-    console.log(count);
-  }, 5000);
-}
+createPost({ title: "Post 3", body: "This is post 3" })
+  .then(getPost)
+  .catch((err) => console.log(err));
